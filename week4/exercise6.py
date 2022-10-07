2eb4b53ce25ab09e1e6923d3d3241974728ba759
Emily <eshen4@jh.edu>
2022-10-07 14:07:57 -0400

    fully commented

--- week4/exercise6.py
+++ week4/exercise6.py
@@ -9,14 +9,12 @@ CBman = np.genfromtxt("CB.qassoc",
                     dtype = None, skip_header = 1, encoding = None,
                     names = ["CHR", "SNP", "BP", "NMISS", "BETA", "SE", "R2",
                                 "T", "P"])
-# print(CBman[1])
-# exit()
-                                
+# generating from text my CB.qassoc file to read through the P values and find the correct SNP                        
 a = np.argmax(-np.log(CBman["P"]))
 #this is the index (row) for the values at our highest -log P, which is our SNP with the highest association
 #print(a)
 b = CBman["SNP"][a]
-#this is the snp id for the largest -log p value
+#this is the snp id for the largest -log p value (what we were trying to find)
 #print(b)
 vcf = vcfParser.parse_vcf("gwas_data/genotypes.vcf")
 # print(vcf[0])
@@ -24,32 +22,47 @@ vcf = vcfParser.parse_vcf("gwas_data/genotypes.vcf")
 # exit()
 #parsing the genotypes vcf -- we will need to match our variable b with a vcf
 for i in vcf:
+#for variables in our parsed vcf...
     if i[2] == b:
+    #if our SNP id (i[2] is the index for the SNP id) is equal to the id we obtained for variable b (associated to most significant P value)
         genotype = dict(zip(vcf[0][9:], i[9:]))
-        #zips the two lists together for the dictionary!
+        #zips the two lists together for the dictionary - vcf header columns 9 and on, with the actual data in columns 9 and on (this is our genotype data!)
 #print(genotype)
 phenotype = np.genfromtxt("gwas_data/CB1908_IC50.txt",
                             dtype = None, skip_header = 1, encoding = None,
                             names = ["FID", "IID", "CB1908_IC50"])        
+#generating from text my phenotypic data
 #print(phenotype)
 ref = []
 het = []
 alt = []
+#establishing three empty lists for the reference, alternate, and heterozygous genotypes
 for i in phenotype:
+#for variable in our phenotype text
     d = str(i["FID"])+"_"+str(i["IID"])
+    #establishing a variable to represent what we're looking for as a string. this is necessary because the vcf for our genotype data wrote the header differently compared to the phenotype data
     if genotype[d] == "0/0":
+    #if the data connected to our string key in our genotype dictionary is equal to "0/0"...
         if i["CB1908_IC50"] != "NA":
+        #another if statement because some of the phenotype data says "NA" instead of an actual value, so this is just saying if our value is NOT equal to that (we don't want NA)
             ref.append(float(i["CB1908_IC50"]))
+            #append the values as floats at our location i in our third column of our phenotype file into our reference list
     elif genotype[d] == "0/1" or genotype[d] == "1/0":
+    #same as above except this or statement is necessary because the vcf file wrote some hets as 1/0 and some as 0/1
         if i["CB1908_IC50"] != "NA":
              het.append(float(i["CB1908_IC50"]))
     elif genotype[d] == "1/1":
         if i["CB1908_IC50"] != "NA":
             alt.append(float(i["CB1908_IC50"]))
 #print(alt)
+
 fig, ax = plt.subplots()
+#establishing the plot
 data = [ref, het, alt]
+#made a list for our data in the order ref, het, alt (previously defined variables)
 ax.boxplot(data, labels = ["ref", "het", "alt"])
+#plots our data variable all next to each other on the same figure axes but with different boxplots for each
 ax.set_title("phenotypic effect size of each genotypic variation")
+#set title for figure
 fig.savefig("exercise6.png")
 plt.close(fig)
--- week4/week4.py
+++ week4/week4.py
@@ -9,6 +9,8 @@ pca = np.genfromtxt("plink.eigenvec",
                     dtype = None, encoding = None,
                     names = ["ID1", "ID2", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6",
                                 "PC7", "PC8", "PC9", "PC10"])
+#generating from text our plink.eigenvec file which was generated through a bash command in terminal
+
 fig, ax = plt.subplots()
 ax.scatter(pca["PC1"], pca["PC2"])
 ax.set_title("genetic relatedness between principal components 1 and 2")
@@ -39,20 +41,29 @@ CBman = np.genfromtxt("CB.qassoc",
 a = np.argmax(-np.log(CBman["P"]))
 aSNP = CBman["SNP"][a]
 print(aSNP)
-
+#this small section is actually for exercise 7 just to get the snp id for CB
 fig, ax = plt.subplots()
 Pval = 10e-05
+#saved given pval as a separate variable
 p = CBman["P"]
+#saved our p values column from our generated file as a variable p to parse through with a for loop later
 col = []
+#empty list for the colors for each sample
 
 for i in p:
+#for variable in our p value column (saved as variable p)
     if i < Pval:
+    #if the p value at the location is less than our set pval...
         col.append('magenta')
+        #add the point to our color list as "magenta"
     else:
         col.append('cyan')
+        #if the P value at the location is greater than or equal to our set p val, add the point to our color list as "cyan"
+#i did this color part by myself!! like completely!! :) 
 # print(col.count('magenta'))
 # exit()
 ax.scatter(np.arange(len(CBman)), -np.log(CBman["P"]), c = col)
+#make our scatter with some variables -- our x is the length of our CBman file spaced out evenly, while our y is the negative log of our p values so that the highest point represents greatest association rather than the lowest (p values normally smaller is more significant). c = col defines the colors of our indvidual points.
 ax.set_title("GWAS of CB1908_IC50")
 ax.set_xlabel("Number of samples")
 ax.set_ylabel("-log of P-values")
@@ -66,7 +77,7 @@ GSman = np.genfromtxt("GS.qassoc",
 b = np.argmax(-np.log(GSman["P"]))
 bSNP = GSman["SNP"][b]
 print(bSNP)
-
+#this small section is actually for exercise 7 just to get the snp id for GS
 fig, ax = plt.subplots()
 Pval = 10e-05
 p = GSman["P"]
